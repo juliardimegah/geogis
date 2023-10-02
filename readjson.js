8493c4473ae70ae8bcb783039dee9cd7e35a478c
juliardimegah <juliardimegah@gmail.com>
2023-10-02 12:18:21 +0700

    Update

--- readjson.js
+++ readjson.js
@@ -1,3 +1,103 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([107.59346907209243,
+                -6.9928984337098825]),
+            zoom: 15
+        })
+    });
+
+    // Mendownload data waypoint, line string, dan polyline
+    const waypointSource = new ol.source.Vector({
+        url: 'https://raw.githubusercontent.com/juliardimegah/geogis.github.io/main/point.json',
+        format: new ol.format.GeoJSON()
+    });
+
+    const lineStringSource = new ol.source.Vector({
+        url: './poligon.json',
+        format: new ol.format.GeoJSON()
+    });
+
+    const polylineSource = new ol.source.Vector({
+        url: 'https://raw.githubusercontent.com/juliardimegah/geogis.github.io/main/poliline.json',
+        format: new ol.format.GeoJSON()
+    });
+
+    // Membuat layer untuk waypoint, line string, dan polyline
+    const waypointLayer = new ol.layer.Vector({
+        source: waypointSource,
+        style: new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 5,
+                fill: new ol.style.Fill({
+                    color: 'blue'
+                })
+            })
+        })
+    });
+
+    const lineStringLayer = new ol.layer.Vector({
+        source: lineStringSource,
+        style: new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'green',
+                width: 2
+            })
+        })
+    });
+
+    const polylineLayer = new ol.layer.Vector({
+        source: polylineSource,
+        style: new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'black',
+                width: 5
+                
+            })
+        })
+    });
+
+    // Menambahkan layer ke peta
+    map.addLayer(waypointLayer);
+    map.addLayer(lineStringLayer);
+    map.addLayer(polylineLayer);
+
+    // Mendapatkan koordinat dari GeoJSON
+    const getCoordinates = (source) => {
+        const features = source.getFeatures();
+        const coordinates = features[0].getGeometry().getCoordinates();
+        return coordinates;
+    };
+
+    // Menampilkan koordinat di dalam tabel
+    waypointSource.once('change', () => {
+        const waypointCoords = getCoordinates(waypointSource);
+        document.getElementById('featureName').textContent = 'Waypoint';
+        document.getElementById('featureType').textContent = 'Point';
+        document.getElementById('featureCoords').textContent = waypointCoords.toString();
+    });
+
+    lineStringSource.once('change', () => {
+        const lineStringCoords = getCoordinates(lineStringSource);
+        document.getElementById('featureName').textContent = 'Line String';
+        document.getElementById('featureType').textContent = 'Line String';
+        document.getElementById('featureCoords').textContent = lineStringCoords.toString();
+    });
+
+    polylineSource.once('change', () => {
+        const polylineCoords = getCoordinates(polylineSource);
+        document.getElementById('featureName').textContent = 'Polyline';
+        document.getElementById('featureType').textContent = 'Polyline';
+        document.getElementById('featureCoords').textContent = polylineCoords.toString();
+    });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
 
@@ -23,7 +123,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     const pointTable = document.getElementById("polygonTable").getElementsByTagName('tbody')[0];
 
-    fetch("https://raw.githubusercontent.com/juliardimegah/geogis.github.io/main/poligon.json") // Ganti "data.json" dengan nama file JSON Anda
+    fetch("./poligon.json") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
@@ -63,3 +163,4 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch(error => console.error("Terjadi kesalahan:", error));
 });
+
